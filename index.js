cc254be9fab1c01129538ab127577af3bc941e8e
Fran1o <pankha994@gmail.com>
2022-05-10 19:08:01 -0300

    subiendo

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const server = app.listen(PORT, () =>{
     console.log(`Servidor escuchando en el puerto ${PORT}`)
 })
 
+
 //routes
 app.use(express.static(__dirname + "/public"))
 app.use('/api', routesProducts)
