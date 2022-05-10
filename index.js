533ae690ed16979bf1941102f76cf596a131063d
Fran1o <pankha994@gmail.com>
2022-05-10 19:00:21 -0300

    subiendo

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express ()
+const routesProducts = require('./routes/productos')
+app.use(express.json())
+
+//empezando servidor
+const PORT = 8080
+const server = app.listen(PORT, () =>{
+    console.log(`Servidor escuchando en el puerto ${PORT}`)
+})
+
+//routes
+app.use(express.static(__dirname + "/public"))
+app.use('/api', routesProducts)
